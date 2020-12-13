c35038a6081fe31eeda76efce217f71dd39825ee
Artem <artem.zolotukhin@movika.com>
2020-12-13 13:34:26 +0300

    Update sdk and simplify

--- sample/app/src/main/java/com/movika/sample/activity/PlayerActivity.kt
+++ sample/app/src/main/java/com/movika/sample/activity/PlayerActivity.kt
@@ -4,35 +4,41 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.movika.sample.R
-import com.movika.sample.gameplayer.PlayerContainer
+import film.interactive.player.sdk.android.base.Config
 import film.interactive.player.sdk.android.base.tools.loader.AsyncMovieBundleLoader
 import film.interactive.player.sdk.android.base.tools.loader.MovieBundleLoader
+import film.interactive.player.sdk.android.base.ui.InteractivePlayerView
 
 class PlayerActivity : AppCompatActivity() {
-
-    private lateinit var playerContainer: PlayerContainer
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fullscreen)
-        playerContainer = findViewById(R.id.playerContainer)
-        val loader : MovieBundleLoader = AsyncMovieBundleLoader()
-        loader.load("https://asazin-cache.cdnvideo.ru/asazin/movika/prod/users/1/movie/10/manifest.json")
-        {
+        val interactivePlayer: InteractivePlayerView = findViewById(R.id.interactivePlayer)
+        val eventContainer: ViewGroup = findViewById(R.id.eventContainer)
+        interactivePlayer.customEventContainer = eventContainer
+        val config = Config(
+            isLoop = true,
+            isDebugMode = false,
+            isShowTimeBar = true,
+            isUseDefaultPlayPauseController = true,
+            isCacheStartPartsOfNextChapters = true,
+            isSeekEnabled = false
+        )
+
+        lifecycle.addObserver(interactivePlayer)
+
+        val loader: MovieBundleLoader = AsyncMovieBundleLoader()
+        loader.load("https://asazin-cache.cdnvideo.ru/asazin/movika/prod/users/1/movie/10/manifest.json") {
             if (it.isComplete) {
                 Handler(Looper.getMainLooper()).post {
-                    playerContainer.conceivePlayer(it.data)
-                    playerContainer.playerView?.onResume()
+                    interactivePlayer.run(it.data!!, config, savedInstanceState)
                 }
             }
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        playerContainer.abortPlayer()
     }
 }
